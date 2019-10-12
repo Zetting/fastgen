09e12a538df31538365821a75ac92e2ec47eb612
zet <linzetian@ppmoney.com>
2019-10-12 18:10:53 +0800

    fix:新增新增项目前端

--- fastgen-web/src/views/generator/module/header.vue
+++ fastgen-web/src/views/generator/module/header.vue
@@ -12,22 +12,87 @@
         @click="to">配置</el-button>
       <eForm ref="form"/>
     </div>
+    <div class="right-item" >
+      <el-select v-model="currencyProjectId" @change="switchProject" style="width: 110px; margin-right:3px;" filterable clearable  placeholder="请选择">
+        <el-option
+          v-for="item in projects"
+          :key="item.path"
+          :label="item.projectName"
+          :value="item.projectId">
+          <span style="float: right">
+            {{ item.projectName }} <i class="el-icon-edit" @click="editProject(item)"></i>
+          </span>
+        </el-option>
+      </el-select>
+      <i class="el-icon-plus" @click="createProject"></i>
+    </div>
+
+    <!--新增项目-->
+    <el-dialog
+      :visible.sync="projectFormVisible"
+      :title="edited ? '编辑项目' : '新增项目'"
+      append-to-body
+      width="350px">
+      <el-form ref="projectForm" :model="projectForm" :rules="projectRules">
+        <el-input v-model="projectForm.projectId" type="hidden" />
+        <el-form-item label="项目名称" prop="projectName">
+          <el-input v-model="projectForm.projectName" placeholder="项目名称"/>
+        </el-form-item>
+        <el-form-item label="项目路径" prop="path">
+          <el-input v-model="projectForm.path" placeholder="项目路径"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button v-if="edited" @click="removeProject">删除</el-button>
+        <el-button v-if="!edited" @click="projectFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitEditProject">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-import { get } from '@/api/genConfig'
+import { get ,update } from '@/api/genConfig'
 import eForm from './form'
 // 查询条件
 export default {
   components: { eForm },
+  data() {
+    return {
+      projects: [],
+      configs: { projects: [] },
+      currencyProjectId: '',
+      projectFormVisible : false,
+      projectForm: { projectId: '', projectName: '', path: '' },
+      projectRules: {
+        projectName: [
+          {required: true, message: '项目名不能为空', trigger: 'blur'}
+        ],
+        path: [
+          {required: true, message: '项目路径不能为空', trigger: 'blur'}
+        ]
+      },
+      edited: false
+    }
+  },
   props: {
     query: {
       type: Object,
       required: true
     }
   },
+  created() {
+    this.initData()
+  },
   methods: {
+    initData() {
+      get().then(res => {
+        var data = res.data
+        this.configs = data
+        this.projects = data.projects == undefined ? [] : data.projects
+      })
+    },
     toQuery() {
       this.$parent.page = 0
       this.$parent.init()
@@ -49,7 +114,88 @@ export default {
         })
       })
       _this.dialog = true
+    },
+
+    /**
+     * 切换项目
+     */
+    switchProject(item) {
+      debugger
+      this.currencyProjectId = item
+    },
+    /**
+     * 删除项目
+     */
+    removeProject() {
+      var index = this.projects.indexOf(this.projectForm)
+      if (index !== -1) {
+        this.projects.splice(index, 1)
+      }
+      this.projectFormVisible = false
+      var newConfigs = this.configs
+      newConfigs.projects = this.projects
+      update(newConfigs).then(res => {
+        this.$notify({
+          title: '删除成功',
+          type: 'success',
+          duration: 1000
+        })
+      })
+    },
+    /**
+     * 编辑项目
+     */
+    editProject(item) {
+      this.edited = true
+      this.projectFormVisible = true;
+      this.projectForm = item
+    },
+    /**
+     * 新增项目
+     */
+    createProject() {
+      this.edited = false
+      this.projectFormVisible = true;
+      this.projectForm = { projectId: Date.now(), projectName: '', path: '' }
+    },
+    /**
+     * 确认新增项目
+     */
+    submitEditProject() {
+      this.$refs['projectForm'].validate((valid) => {
+        if (valid) {
+          this.projectFormVisible = false
+          if(!this.edited){
+            this.projects.push(this.projectForm)
+          }else {
+            var index = this.projects.indexOf(this.projectForm)
+            if (index !== -1) {
+              this.projects[index]=this.projectForm;
+            }
+          }
+          var newConfigs = this.configs
+          newConfigs.projects = this.projects
+          update(newConfigs).then(res => {
+            this.$notify({
+              title: '操作成功',
+              type: 'success',
+              duration: 1000
+            })
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
 </script>
+
+
+<style scoped>
+  .right-item{
+    float:right;
+    border: none;
+    padding-right: 20px;
+  }
+</style>
